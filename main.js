8f3650f8a475496a0c96b3f42e904fe589231da3
coder <2353224481@qq.com>
2016-09-08 12:21:59 +0800

    Test data init

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ $(function(){
     //点击开始计算的时候
     $('#go').click(function(){
         var table = document.querySelector('table');
-        shudu = new Shudu(table);
+        shudu = new Sudoku(table);
     });
-    /*测试数据
+    //Test data set below,or you can remove it
     var nodes = [
         [0,0,0,6,9,0,0,8,1],
         [3,1,9,8,0,0,0,4,2],
@@ -59,30 +59,29 @@ $(function(){
             }
         }
     }
-    */
+    //End of Test data set
 });
 
-function Shudu(table){
+function Sudoku(table){
     this.table = table;
     this.data = new Object();
     this.init();
-    var zeroNum = 0;
-    var i = 0;
+    //求解    
     while(this.calcu()){};
 }
 //初始化
-Shudu.prototype.init = function(){
+Sudoku.prototype.init = function(){
     var self = this;
     $(self.table).find('tr').each(function(i,e){
         var arr = [];
         $(e).find('td').each(function(ii,ee){
             arr.push($(ee).html() == ""?0:parseInt($(ee).html()));
-            self.data[i] = arr;
         });
+        self.data[i] = arr;
     });
 }
 //计算，如果填充成功，返回true，否则返回false，返回false说明计算结束，如果仍有空格未填，则说明此数独不能找到明确解
-Shudu.prototype.calcu = function(){
+Sudoku.prototype.calcu = function(){
     var self = this;
     var allNum = [1,2,3,4,5,6,7,8,9];
     for(var i = 0; i < 9; i++){
@@ -107,7 +106,7 @@ Shudu.prototype.calcu = function(){
     return false;
 }
 //求X方向数集
-Shudu.prototype.xdirectionAll = function(x,y){
+Sudoku.prototype.xdirectionAll = function(x,y){
     var self = this;
     var arr = [];
     for(var j = 0; j < 9; j++){
@@ -119,7 +118,7 @@ Shudu.prototype.xdirectionAll = function(x,y){
     return arr;
 }
 //求Y方向数集
-Shudu.prototype.ydirectionAll = function(x,y){
+Sudoku.prototype.ydirectionAll = function(x,y){
     var self = this;
     var arr = [];
     for(var j = 0; j < 9; j++){
@@ -131,7 +130,7 @@ Shudu.prototype.ydirectionAll = function(x,y){
     return arr;
 }
 //求方格数集
-Shudu.prototype.RectAll = function(x,y){
+Sudoku.prototype.RectAll = function(x,y){
     var self = this;
     var xModRet = x % 3;
     var yModRet = y % 3;
@@ -150,7 +149,7 @@ Shudu.prototype.RectAll = function(x,y){
 }
 
 //设置某格值的显示
-Shudu.prototype.ShowNum = function(x,y,num){
+Sudoku.prototype.ShowNum = function(x,y,num){
     var self = this;
     $(self.table).find('tr').eq(x).find('td').eq(y).html(num);
 }
